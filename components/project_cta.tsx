f348e7b7bef33f0504c0dcd7b532b2fc7b5b4786
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-06-23 17:00:19 +0300

    Improving UX 3

--- components/project_cta.tsx
+++ components/project_cta.tsx
@@ -1,31 +1,14 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import Arrow from "./arrow";
 
 export default function ProjectCTA() {
-  const [postion, setPostion] = useState<number>(0);
-
-  const handleScroll = () => {
-    const scrollPosition = window.scrollY;
-    setPostion(scrollPosition);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className="text-white grid max-[820px]:hidden gap-y-8 grid-rows-[2.5fr,2fr,1fr]">
       <a
         target="_blank"
         href="https://soundly-peach.vercel.app/"
-        className={`block ${
-          postion > 160 ? "Fade" : "opacity-0"
-        } cursor-pointer relative h-full py-9 pl-9 pr-5 text-white rounded-2xl  min-w-[312px] bg-BP `}
+        className="block 
+         cursor-pointer relative h-full py-9 pl-9 pr-5 text-white rounded-2xl  min-w-[312px] bg-BP "
       >
         <h1 className="font-bold text-5xl">Try It Now</h1>
         <Arrow
@@ -38,9 +21,7 @@ export default function ProjectCTA() {
       <a
         target="_blank"
         href="https://github.com/George-F-Ramzi/soundly"
-        className={`block ${
-          postion > 240 ? "Fade" : "opacity-0"
-        } cursor-pointer relative h-full py-9 pl-9 pr-5 text-white rounded-2xl  min-w-[312px] bg-BS `}
+        className={`block cursor-pointer relative h-full py-9 pl-9 pr-5 text-white rounded-2xl  min-w-[312px] bg-BS `}
       >
         <h1 className="font-bold text-5xl">Code</h1>
         <Arrow
@@ -53,9 +34,7 @@ export default function ProjectCTA() {
       <a
         target="_blank"
         href="https://www.figma.com/file/nohasksUb7HzWLOFQ8JiPV/Soundly----music-sharing-platfrom?type=design&mode=design&t=xZSFtb9YZf0uSdyx-0"
-        className={`block ${
-          postion > 240 ? "Fade" : "opacity-0"
-        } cursor-pointer h-full rounded-2xl min-w-[312px] bg-transparent border border-[#B8B8B8] `}
+        className={`block  cursor-pointer h-full rounded-2xl min-w-[312px] bg-transparent border border-[#B8B8B8] `}
       >
         <div className="w-full h-full flex pl-9 pr-5 items-center  rounded-2xl">
           <h1 className="font-bold text-[#B8B8B8] text-[32px] grow">Design</h1>
