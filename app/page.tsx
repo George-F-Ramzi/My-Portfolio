4a79eb946c61fa8fbdc190af361547a35c7dcdf0
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-06-25 13:07:18 +0300

    finished the whole portfolio ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import LandingHero from "@/components/landing_hero";
+import ContactActions from "@/components/micro_components/contact_actions";
 import ProjectsSeparator from "@/components/micro_components/projects_separator";
 import NavBar from "@/components/nav_bar";
 import SkillsSection from "@/components/skills_section";
@@ -17,6 +18,12 @@ export default function Home() {
         Skills
       </h2>
       <SkillsSection />
+      <h2 className="max-[772px]:mt-[56px] mt-32 max-[772px]:mb-12 mb-24 max-[772px]:text-[32px] max-[420px]:text-[24px] leading-LH font-bold text-[64px]">
+        LET`S TALK ABOUT
+        <br />
+        THE NEXT BIG THING.
+      </h2>
+      <ContactActions />
     </main>
   );
 }
